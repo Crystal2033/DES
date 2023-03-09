8ade7f3e5af8d6ed067cadcecd60da972ed053c0
Pavel <kulikov-pavlik@bk.ru>
2023-03-09 21:06:58 +0300

    Made initial permutation!

--- DES/HelpFunctions/CryptSimpleFunctions.cs
+++ DES/HelpFunctions/CryptSimpleFunctions.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace DES.HelpFunctions
 {
-    internal class CryptSimpleFunctions
-
-        
+    internal class CryptSimpleFunctions  
     {
-        private static void showBinaryView(in byte[] viewBytes) {
+        private const byte BITS_IN_BYTE = 8;
+        private static void showBinaryView(in byte[] viewBytes, in string message) {
+            Console.WriteLine(message);
             for (int i = 0; i < viewBytes.Length; i++)
             {
                 Console.Write(Convert.ToString(viewBytes[i], 2).PadLeft(8, '0'));
@@ -18,11 +18,24 @@ namespace DES.HelpFunctions
             }
             Console.WriteLine();
         }
+
+
         public static void Permutation(ref byte[] bytes, in byte[] pBlock)
         {
-            showBinaryView(bytes);
+            showBinaryView(bytes, "Start bytes view");
+            byte[] result = new byte[bytes.Length];
+            for (byte i = 0; i < pBlock.Length; i++)
+            {
+                byte currBlockIndex = (byte)((pBlock[i] - 1) / BITS_IN_BYTE);
 
+                byte currBit = (byte)((bytes[currBlockIndex] >> ((currBlockIndex + 1) * BITS_IN_BYTE - pBlock[i]) & 1));
 
+                result[i / BITS_IN_BYTE] = (byte)(result[i / BITS_IN_BYTE] | (currBit << BITS_IN_BYTE - (i % BITS_IN_BYTE + 1)));
+            }
+            bytes = result;
+            showBinaryView(bytes, "Result bytes");
+
+            
         }
     }
 }
