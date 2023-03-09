242655acabb68226cff5b1420af4df6d95db4e80
Pavel <kulikov-pavlik@bk.ru>
2023-03-09 19:25:50 +0300

    Add project files.

---
+++ DES/HelpFunctions/CryptSimpleFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DES.HelpFunctions
+{
+    internal class CryptSimpleFunctions
+
+        
+    {
+        private static void showBinaryView(in byte[] viewBytes) {
+            for (int i = 0; i < viewBytes.Length; i++)
+            {
+                Console.Write(Convert.ToString(viewBytes[i], 2).PadLeft(8, '0'));
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        public static void Permutation(ref byte[] bytes, in byte[] pBlock)
+        {
+            showBinaryView(bytes);
+
+
+        }
+    }
+}
---
+++ DES/Program.cs
@@ -0,0 +1,14 @@
+﻿using DES.HelpFunctions;
+    
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte[] myTextBytes = new byte[8];
+        for (byte i = 0; i < myTextBytes.Length; i++) {
+            myTextBytes[i] = i;
+        }
+        byte[] pBlock = new byte[64];
+        CryptSimpleFunctions.Permutation(ref myTextBytes, pBlock);
+    }
+}
