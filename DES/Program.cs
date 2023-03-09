932426f6712bd5a63e655bddf73ef4f982c4ed5c
Pavel <kulikov-pavlik@bk.ru>
2023-03-09 22:03:39 +0300

    save last not important changes

--- DES/Program.cs
+++ DES/Program.cs
@@ -5,11 +5,6 @@ internal class Program
     private static void Main(string[] args)
     {
         byte[] myTextBytes = new byte[8] {117,85,93,53, 78,146,200,190};
-        //byte[] myTextBytes = new byte[8] { 1,2,3,4,5,6,'A','B','C','D',1,3,2,5,3,6 };
-        Random random = new Random();
-        //for (byte i = 0; i < myTextBytes.Length; i++) {
-        //    myTextBytes[i] = (byte)random.Next(0, 255);
-
  
         CryptSimpleFunctions.Permutation(ref myTextBytes, DESStandartBlocks.IPBlock);
 
